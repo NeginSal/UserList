f04020d6518adb361b4f4caed110c3cae77954b0
NeginSal <neginsaljooghi@gmail.com>
2021-07-27 17:56:18 +0430

    add users component and css for it

--- pages/users/index.js
+++ pages/users/index.js
@@ -1,7 +1,24 @@
-const Users = () => {
+import styles from '../../styles/users.module.css'
+
+export const getStaticProps = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const data =await res.json();
+
+    return {
+        props: { users : data }
+    }
+}
+const Users = ({users}) => {
     return (
         <div>
             <h1>All Users</h1>
+            {users.map(user => (
+                <div key={user.id}>
+                    <a className={styles.single}>
+                        <h3>User {user.id}</h3>
+                    </a>
+                </div>
+            ))}
         </div>
      );
 }

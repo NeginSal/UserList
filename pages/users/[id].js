a05790a566c2bb98966a39da46917871c6ade4ec
NeginSal <neginsaljooghi@gmail.com>
2021-07-27 19:01:11 +0430

    edit id.js

--- pages/users/[id].js
+++ pages/users/[id].js
@@ -1,9 +1,37 @@
-const Details = () => {
-    return (
-        <div>
-            <h1>Details Page</h1>
-        </div>
-     );
-}
+export const getStaticPaths = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const data = await res.json();
 
-export default Details;
+    const paths = data.map(user => {
+      return {
+        params: { id: user.id.toString() }
+      }
+    })
+
+    return {
+      paths,
+      fallback: false
+    }
+  }
+
+  export const getStaticProps = async (context) => {
+    const id = context.params.id;
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts/' + id);
+    const data = await res.json();
+
+    return {
+      props: { user: data }
+    }
+  }
+
+  const Details = ({ user }) => {
+    return (
+      <div>
+        <h1>User id : { user.id }</h1>
+        <p>title : { user.title }</p>
+        <p>body : { user.body }</p>
+      </div>
+    );
+  }
+
+  export default Details;

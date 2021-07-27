180c13e75d57252c93b95a153ba1afcb4dc0ae52
NeginSal <neginsaljooghi@gmail.com>
2021-07-27 15:56:11 +0430

    add Navbar

---
+++ comps/Navbar.js
@@ -0,0 +1,15 @@
+const Navbar = () => {
+    return (
+        <nav>
+            <div className="logo">
+                <h1>Users List</h1>
+            </div>
+            <a>Home</a>
+            <a>About</a>
+            <a>User List</a>
+
+        </nav>
+     );
+}
+
+export default Navbar;
